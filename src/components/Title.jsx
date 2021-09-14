15f534827556790bb6aec3e3fbb41d12e4c19bc3
Brittney Jackson <brnjackson@yahoo.com>
2021-09-13 21:37:37 -0400

    completed classwork

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Title.css'
+
+const Title = () => {
+return (
+  <div className="page">
+    <h1 className="title">Brittney is a master coder!</h1>
+    <p className="intro">I like pizza and taking naps.</p>
+  </div>
+    )
+}
+
+export default Title;
